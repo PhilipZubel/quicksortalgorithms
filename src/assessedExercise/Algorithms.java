f237f40e41654deda0391b6e5d749a0516c46fbf
“Philip <2479229z@student.gla.ac.uk>
2021-02-18 15:37:57 +0000

    generateArray method added

--- src/assessedExercise/Algorithms.java
+++ src/assessedExercise/Algorithms.java
@@ -1,5 +1,9 @@
 package assessedExercise;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Algorithms {
 
 	/*
@@ -186,29 +190,57 @@ public class Algorithms {
 		return new int[] { i + 1, sameAsPivot };
 	}
 
+	// swaps two indices of array aa
 	private static void swap(int[] aa, int idx1, int idx2) {
 		int temp = aa[idx1];
 		aa[idx1] = aa[idx2];
 		aa[idx2] = temp;
+	}
+	
+	// This function generates random distinct numbers and uses
+	// QuadraticTimeQuickSort() to reorder the numbers in a way 
+	// that quicksortc would run with complexity O(n^2)
+	public static int[] generateArray(int len) {
+		Set<Integer> set = new HashSet<Integer>();
+		Random rand = new Random(); 
+		// Create random integers and add them to the set
+		while(set.size() < len) {
+			int number = rand.nextInt();
+			set.add(number);
+		}
+		// convert the set to array of int values
+		int[] aa = set.stream() 
+	            .mapToInt(Integer::intValue) 
+	            .toArray();
+		
+		// reorder indices of array aa
+		QuadraticTimeQuickSort(aa, 0, len-1);
+		return aa;
 	}
 
 	// This function sorts an array in such a way that
 	// quicksortc would run with complexity O(n^2)
 	// [6,2,7,9,8,1,3,5,0,4] -> [0,2,4,6,8,1,3,5,7,9]
 	// [3,6,1,2,8,7,0,4,5] -> [0,2,4,6,8,1,3,5,7]
-	public static void QuadraticTimeQuickSort(int[] aa, int p, int r) {
+	private static void QuadraticTimeQuickSort(int[] aa, int p, int r) {
 		int n = aa.length;
+		// sort the array
 		Algorithms.insertionsort(aa, 0, n - 1);
 		for (int i = 1; i < n / 2; i += 2) {
 			if (n % 2 == 0) {
+				// if array has an even number of elements
 				swap(aa, i, n - i - 1);
 			} else {
+				// if array has an odd number of elements
 				swap(aa, i, n - i);
 			}
 
 		}
+		// sort the two halves of the array
 		Algorithms.insertionsort(aa, 0, (n - 1) / 2);
 		Algorithms.insertionsort(aa, (n - 1) / 2 + 1, n - 1);
 	}
+	
+	
 
 }
--- src/assessedExercise/AssessedExercise.java
+++ src/assessedExercise/AssessedExercise.java
@@ -8,7 +8,7 @@ public class AssessedExercise {
 
 	public static void main(String[] args) throws IOException {
 
-//		Exercise 1
+//		// Exercise 1
 //		int[] aa = Test.importArray("int10.txt");
 //		int cutoff = 5;
 //		int p =0;
@@ -23,88 +23,41 @@ public class AssessedExercise {
 //		Algorithms.quicksortD(aa, p, r);
 
 
-//		Exercise 2
-//		import file to array; make sure the file is in the numbers directory 
-		int[] aa = Test.importArray("int10.txt");
-//		checks if aa array is sorted
-		boolean isTrue = Test.isSorted(aa);
-//		returns the time needed to sort aa using quicksortA
-//		valid algorithms: "quicksorta", "quicksortc", "quicksortd", "merge", "insertion"
-		double time = Test.getTimeForArray(aa, "quicksorta");
-// 		returns the time needed to sort the array in the specified file
-		time = Test.getTimeForFile("int10.txt", "quicksorta");
-//      returns the times needed to sort the arrays for each file
-		double[] times = Test.getTimes("quicksorta");
-// 		returns running times for all files for all algorithms stated earlier
-		double[][] allTimes = Test.getAllTimes();
-//		stores the values of Test.getAllTimes() in CSV format
-		Test.storeTimesCSV();
-// --------------------------------------------------------------------------------------
-//		returns the time needed to sort using quicksortB with a cutoff value = 5
-		time = Test.getTimeQuicksortBForFile("int10.txt", 5);
-//		returns the time needed to sort aa using quicksortB for a cutoff value = 5
-		times = Test.getTimesQuicksortB(5);
-// 		returns running times for all files for quicksortB for cutoffs between 1-24 
-		allTimes = Test.getAllTimesQuicksortB();
-//		stores the values of Test.getAllTimesQuicksortB() in CSV format
-		Test.storeTimesQuicksortBCSV();
+//		// Exercise 2
+//      // import file to array; make sure the file is in the numbers directory 
+//		int[] aa = Test.importArray("int10.txt");
+//		// checks if aa array is sorted
+//		boolean isTrue = Test.isSorted(aa);
+//		// returns the time needed to sort aa using quicksortA
+//		// valid algorithms: "quicksorta", "quicksortc", "quicksortd", "merge", "insertion"
+//		double time = Test.getTimeForArray(aa, "quicksorta");
+// 		// returns the time needed to sort the array in the specified file
+//		time = Test.getTimeForFile("int10.txt", "quicksorta");
+//      // returns the times needed to sort the arrays for each file
+//		double[] times = Test.getTimes("quicksorta");
+// 		// returns running times for all files for all algorithms stated earlier
+//		double[][] allTimes = Test.getAllTimes();
+//		// stores the values of Test.getAllTimes() in CSV format in results.csv
+//		Test.storeTimesCSV();
+//// --------------------------------------------------------------------------------------
+//		// returns the time needed to sort using quicksortB with a cutoff value = 5
+//		time = Test.getTimeQuicksortBForFile("int10.txt", 5);
+//		// returns the time needed to sort aa using quicksortB for a cutoff value = 5
+//		times = Test.getTimesQuicksortB(5);
+// 		// returns running times for all files for quicksortB for cutoffs between 1-24 
+//		allTimes = Test.getAllTimesQuicksortB();
+//		// stores the values of Test.getAllTimesQuicksortB() in CSV format in resultsb.csv
+//		Test.storeTimesQuicksortBCSV();
 		
 		
+// 		// Exercise 3
+//		// generate array of length 20000
+//		int[] aa = Algorithms.generateArray(20000);
+//		// calculate running time for median quicksort
+//		double time = Test.getTimeForArray(aa, "quicksortc");
+//		System.out.println(time);
+
 		
-		//Test.storeTimesCSV();
-		//Test.storeTimesQuicksortBCSV();
-		
-//		String[] files = Test.getFileNames();
-//		System.out.println("Files:");
-//		System.out.println(Arrays.toString(files));
-//		
-//		double[] testTime = Test.getTimes("quicksorta");
-//		System.out.println("quicksortA");
-//		System.out.println(Arrays.toString(testTime));
-//		
-//		double [] testTime = Test.getTimesQuicksortB(11);
-//		System.out.println("quicksortB 11");
-//		System.out.println(Arrays.toString(testTime));
-//		
-//		double [] testTime = Test.getTimes("quicksortc");
-//		System.out.println("quicksortC");
-//		System.out.println(Arrays.toString(testTime));
-		
-//		double[] testTime = Test.getTimes("quicksortd");
-//		System.out.println("quicksortD");
-//		System.out.println(Arrays.toString(testTime));
-//		
-//		testTime = Test.getTimes("merge");
-//		System.out.println("mergesort");
-//		System.out.println(Arrays.toString(testTime));
-//		
-//    	double [] testTime = Test.getTimes("insertion");
-//		System.out.println("insertion");
-//		System.out.println(Arrays.toString(testTime));
-		
-//		finding best value for quicksortB 
-//		double[][] testTimes = new double[19][8];
-//		double[] testTime;
-//		for(int i=2; i<20; i++) {
-//			testTime = Test.getTimesQuicksortB(i);
-//			System.out.println(i+": "+Arrays.toString(testTime));
-//			//testTimes[i-2] = testTime;
-//		}
-		
-//		System.out.println("Sorting");
-//		int[] aa = Test.importArray("int20k.txt");
-//		Algorithms.QuadraticTimeQuickSort(aa, 0, aa.length-1);
-//		Test.measureSortingTimeArray(aa, "quicksortc");
-//		
-//		
-//		
-}
-//	
-//	public static void swap(int[] aa , int idx1, int idx2) {
-//		int temp = aa[idx1];
-//		aa[idx1] = aa[idx2];
-//		aa[idx2] = temp;		
-//	}
-	
+	}
 
 }
